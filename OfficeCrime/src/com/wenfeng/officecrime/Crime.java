fad82697e0cd46df57d04ed7ab0df49e9be645e5
Elric Gao <elricfeng@gmail.com>
2014-11-22 21:28:02 +0100

    Add some widgets
    1. modify the crime activity
    2. show date with SimpleDateFormat()

--- OfficeCrime/src/com/wenfeng/officecrime/Crime.java
+++ OfficeCrime/src/com/wenfeng/officecrime/Crime.java
@@ -1,14 +1,18 @@
 package com.wenfeng.officecrime;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
 	private UUID crimeId;
 	private String title;
+	private Date date;
+	private boolean solved;
 	
 	public Crime() {
 		crimeId = UUID.randomUUID();
+		date = new Date();
 	}
 
 	public String getTitle() {
@@ -21,6 +25,22 @@ public class Crime {
 
 	public UUID getCrimeId() {
 		return crimeId;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public boolean isSolved() {
+		return solved;
+	}
+
+	public void setSolved(boolean solved) {
+		this.solved = solved;
 	}
 	
 	
--- OfficeCrime/src/com/wenfeng/officecrime/CrimeFragment.java
+++ OfficeCrime/src/com/wenfeng/officecrime/CrimeFragment.java
@@ -1,5 +1,9 @@
 package com.wenfeng.officecrime;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -7,11 +11,17 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 	Crime crime;
 	EditText editTextCrimeTitle;
+	Button buttonCrimeDate;
+	CheckBox checkBoxCrimeSolved;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,10 +29,13 @@ public class CrimeFragment extends Fragment {
 		crime = new Crime();
 	}
 
+	@SuppressLint("SimpleDateFormat")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_crime, container, false);
+		
+		// title editText
 		editTextCrimeTitle = (EditText) view.findViewById(R.id.edittext_crime_title);
 		editTextCrimeTitle.addTextChangedListener(new TextWatcher() {
 			
@@ -35,13 +48,27 @@ public class CrimeFragment extends Fragment {
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {
 				// TODO Auto-generated method stub
-				
 			}
 			
 			@Override
 			public void afterTextChanged(Editable s) {
 				// TODO Auto-generated method stub
 				
+			}
+		});
+		
+		// date button
+		buttonCrimeDate = (Button) view.findViewById(R.id.button_crime_date);
+		buttonCrimeDate.setText(new SimpleDateFormat("EEEE, MMM dd, yyyy").format(crime.getDate()));
+		buttonCrimeDate.setEnabled(false);
+		
+		// solved checkBox
+		checkBoxCrimeSolved = (CheckBox) view.findViewById(R.id.checkbox_crime_solved);
+		checkBoxCrimeSolved.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				crime.setSolved(isChecked);
 			}
 		});
 		
